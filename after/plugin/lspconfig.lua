046a4719fd25e6671ac70ebdb2f897bb01338e25
KenCorp <kennethwarui35@gmail.com>
2024-02-29 13:25:56 +0300

    added go config

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -7,3 +7,58 @@ local lspconfig = require('lspconfig')
 lspconfig.tsserver.setup({})
 lspconfig.pyright.setup({})
 
+local cmp_nvim_lsp_setup, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not cmp_nvim_lsp_setup then return end
+
+local capabilities = cmp_nvim_lsp.default_capabilities()
+
+lspconfig['gopls'].setup {
+  capabilities = capabilities,
+  settings = {
+    gopls = {
+      analyses = {
+        assign = true,
+        atomic = true,
+        bools = true,
+        composites = true,
+        copylocks = true,
+        deepequalerrors = true,
+        embed = true,
+        errorsas = true,
+        fieldalignment = true,
+        httpresponse = true,
+        ifaceassert = true,
+        loopclosure = true,
+        lostcancel = true,
+        nilfunc = true,
+        nilness = true,
+        nonewvars = true,
+        printf = true,
+        shadow = true,
+        shift = true,
+        simplifycompositelit = true,
+        simplifyrange = true,
+        simplifyslice = true,
+        sortslice = true,
+        stdmethods = true,
+        stringintconv = true,
+        structtag = true,
+        testinggoroutine = true,
+        tests = true,
+        timeformat = true,
+        unmarshal = true,
+        unreachable = true,
+        unsafeptr = true,
+        unusedparams = true,
+        unusedresult = true,
+        unusedvariable = true,
+        unusedwrite = true,
+        useany = true,
+      },
+      hoverKind = "FullDocumentation",
+      linkTarget = "pkg.go.dev",
+      usePlaceholders = true,
+      vulncheck = "Imports",
+    },
+  },
+}
